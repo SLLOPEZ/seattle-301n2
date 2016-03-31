c38238100bd1a3b5481178ef212e6ad4b178e358
Shruti Arora <shruti.arora89@icloud.com>
2016-03-30 21:05:35 -0700

    SPA PAGE - ToDO's DONE

---
+++ class-11-spa-routing/pair-assignment/shruti-stephanie11/scripts/articleController.js
@@ -0,0 +1,18 @@
+(function(module) {
+  var articlesController = {};
+
+  // DONE: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
+
+  Article.createTable();
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // code that used to be in index.html.
+  // Also be sure to hide all the main section elements, and reveal the #articles section:
+  Article.fetchAll(articleView.initIndexPage);
+
+  articlesController.index = function() {
+    $('main > section').hide();
+    $('#articles').fadeIn();
+  };
+
+  module.articlesController = articlesController;
+})(window);
