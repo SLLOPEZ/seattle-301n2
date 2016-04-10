6240f0021b79e92f8bec0f055eac583c9a185fac
Stephanie Lopez <lopezlstephanie@gmail.com>
2016-04-10 12:22:34 -0700

    completed the what does this method do portion of the comments

---
+++ class-14-managing-state/pair-assignment/shruti-steph/scripts/articleController.js
@@ -0,0 +1,63 @@
+(function(module) {
+  var articlesController = {};
+
+  Article.createTable();  // Ensure the database table is properly initialized
+
+  articlesController.index = function(ctx, next) {
+    articleView.index(ctx.articles);
+  };
+
+  // COMMENT:What is it's execution path?
+  // This method loads articles with the specific ID the user selected.
+  //
+  articlesController.loadById = function(ctx, next) {
+    var articleData = function(article) {
+      ctx.articles = article;
+      next();
+    };
+
+    Article.findWhere('id', ctx.params.id, articleData);
+  };
+
+  // COMMENT: What is it's execution path?
+  // This method loads articles with the specific author name the user selected.
+  //
+  articlesController.loadByAuthor = function(ctx, next) {
+    var authorData = function(articlesByAuthor) {
+      ctx.articles = articlesByAuthor;
+      next();
+    };
+
+    Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
+  };
+
+  // COMMENT: What is it's execution path?
+  // This method loads articles with the specific category the user selected.
+  articlesController.loadByCategory = function(ctx, next) {
+    var categoryData = function(articlesInCategory) {
+      ctx.articles = articlesInCategory;
+      next();
+    };
+
+    Article.findWhere('category', ctx.params.categoryName, categoryData);
+  };
+
+  // COMMENT: What is it's execution path?
+  // This method loads all the articles.
+  articlesController.loadAll = function(ctx, next) {
+    var articleData = function(allArticles) {
+      ctx.articles = Article.all;
+      next();
+    };
+
+    if (Article.all.length) {
+      ctx.articles = Article.all;
+      next();
+    } else {
+      Article.fetchAll(articleData);
+    }
+  };
+
+
+  module.articlesController = articlesController;
+})(window);
