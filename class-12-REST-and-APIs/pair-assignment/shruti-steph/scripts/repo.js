d91ef9dcd9c46fc9f95ae090445523f7e3e64725
Shruti Arora <shruti.arora89@icloud.com>
2016-04-11 22:03:22 -0700

    class-12 implementation

---
+++ class-12-REST-and-APIs/pair-assignment/shruti-steph/scripts/repo.js
@@ -0,0 +1,30 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  repos.requestRepos = function(callback) {
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax({
+      url: 'https://api.github.com/user/repos',
+      type: 'GET',
+      headers: {
+        'Authorization': 'token ' + 'd3044975093e6f7700567d526548dae07167ffac'
+      },
+      success: function (data, message , xhr) {
+        repos.all = data;
+      }
+    })
+    .done(callback);
+  };
+
+  // DONE: Model method that filters the full collection for repos with a particular attribute.
+  // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
